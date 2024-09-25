673d55435d72bfaca149b895e065f7266b99bedd
deanyang122 <i64991189@gs.ncku.edu.tw>
2024-09-25 23:26:25 +0800

    init project

---
+++ src/main/java/com/example/clockin/ClockinApplication.java
@@ -0,0 +1,13 @@
+package com.example.clockin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClockinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClockinApplication.class, args);
+    }
+
+}
